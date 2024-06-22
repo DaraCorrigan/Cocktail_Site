7a4842ecb3e5d482f5833cb9f6a0b2d884930a5a
acegoal07 <39467245+acegoal07@users.noreply.github.com>
2024-06-22 15:45:34 +0100

    Tweaks
    - Replaces flex view for a grid in css
    - Runs a standardiser and a formatter in index.js file
    - Adds link rel definition and removes duplicate id from index.html

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,84 +2,84 @@
 const searchForm = document.getElementById("search-form");
 
 function performSearch(event) {
-    event.preventDefault();
-    const searchTerm = document.getElementById("search-input").value.trim();
-    console.log("Searching for:", searchTerm);
+  event.preventDefault();
+  const searchTerm = document.getElementById("search-input").value.trim();
+  console.log("Searching for:", searchTerm);
 }
 
 searchForm.addEventListener("submit", performSearch);
 
 // CLEAR SEARCH BAR ON REFRESH //
-            const searchInput = document.getElementById("search-input");
-            
-            window.addEventListener("load", function() {
-                searchInput.value = "";
-            });
+const searchInput = document.getElementById("search-input");
+
+window.addEventListener("load", function () {
+  searchInput.value = "";
+});
 
 // API //
 window.addEventListener("load", function () {
-  var searchButton = document.querySelector("#search-button");
+  const searchButton = document.querySelector("#search-button");
   searchButton.addEventListener("click", searchApi);
 });
 
 function searchApi() {
-  var searchInput = document.querySelector("#search-input").value;
+  const searchInput = document.querySelector("#search-input").value;
   console.log(searchInput);
 
   // api link is www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita
-  var apiLink =
-    "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=" + searchInput; 
-    
+  const apiLink = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchInput}`;
+
   console.log(apiLink);
   fetch(apiLink)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
-      var drinks = data.drinks;
+      const drinks = data.drinks;
       console.log(drinks);
-      var outputDiv = document.querySelector("#output");
+      const outputDiv = document.querySelector("#output");
       while (outputDiv.firstChild) {
         outputDiv.removeChild(outputDiv.firstChild);
       }
-      for (var i = 0; i < drinks.length; i++) {
-        var drink = drinks[i];
-        var drinkName = drink.strDrink;
-        var drinkImage = drink.strDrinkThumb;
-        var drinkId = drink.idDrink;
-        var drinkInstructions = drink.strInstructions;
-        var drinkIngredients = [];
+
+      drinks.forEach(function (drink) {
+        const drinkName = drink.strDrink;
+        const drinkImage = drink.strDrinkThumb;
+        const drinkId = drink.idDrink;
+        const drinkInstructions = drink.strInstructions;
+        const drinkIngredients = [];
+
+        console.log(drink);
 
         // get the ingredients and measurements and concatenate them
-        for (var j = 1; j <= 15; j++) {
-          var ingredient = drink["strIngredient" + j];
-          var measure = drink["strMeasure" + j];
+        for (let j = 1; j <= 15; j++) {
+          const ingredient = drink["strIngredient" + j];
+          const measure = drink["strMeasure" + j];
           if (ingredient) {
-            drinkIngredients.push(measure + " " + ingredient);
+            drinkIngredients.push(`${measure} ${ingredient}`);
           }
         }
-        var drinkDiv = document.createElement("div");
-        var drinkNameElement = document.createElement("h2");
+        const drinkDiv = document.createElement("div");
+        const drinkNameElement = document.createElement("h2");
         drinkNameElement.textContent = drinkName;
-        var drinkImageElement = document.createElement("img");
+        const drinkImageElement = document.createElement("img");
         drinkImageElement.src = drinkImage;
         drinkImageElement.alt = drinkName;
-        var drinkInstructionsElement = document.createElement("p");
+        const drinkInstructionsElement = document.createElement("p");
         drinkInstructionsElement.textContent = drinkInstructions;
         drinkDiv.appendChild(drinkNameElement);
         drinkDiv.appendChild(drinkImageElement);
         drinkIngredients.forEach(function (ingredient) {
-          var drinkIngredientElement = document.createElement("p");
+          const drinkIngredientElement = document.createElement("p");
           drinkIngredientElement.textContent = ingredient;
           drinkDiv.appendChild(drinkIngredientElement);
           drinkNameElement.classList.add("drinkName");
           drinkIngredientElement.classList.add("drinkIngredients");
           drinkInstructionsElement.classList.add("drinkInstructions");
           drinkDiv.classList.add("drinkDiv");
-
         });
         drinkDiv.appendChild(drinkInstructionsElement);
         outputDiv.appendChild(drinkDiv);
-      }
+      });
     });
 }
