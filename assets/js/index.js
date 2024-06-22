86335ff9c5a38571534f1b6bd5ba7b75fe1e2f13
acegoal07 <39467245+acegoal07@users.noreply.github.com>
2024-06-22 20:10:15 +0100

    Random

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,27 +1,27 @@
 // SEARCH BAR ENTER BUTTON //
-const searchForm = document.getElementById("search-form");
+const searchForm = document.querySelector("#search-form");
 
 function performSearch(event) {
   event.preventDefault();
-  const searchTerm = document.getElementById("search-input").value.trim();
+  const searchTerm = document.querySelector("#search-input").value.trim();
   console.log("Searching for:", searchTerm);
 }
 
 searchForm.addEventListener("submit", performSearch);
 
-// CLEAR SEARCH BAR ON REFRESH //
-const searchInput = document.getElementById("search-input");
-
+// ON LOAD
 window.addEventListener("load", function () {
-  searchInput.value = "";
+  // Clear search bar on refresh
+  document.querySelector("#search-input").value = "";
+  // Text search
+  document.querySelector("#search-button").addEventListener("click", searchApi);
+  // Random search
+  document.querySelector("#random-button").addEventListener("click", randomApi);
 });
 
-// API //
-window.addEventListener("load", function () {
-  const searchButton = document.querySelector("#search-button");
-  searchButton.addEventListener("click", searchApi);
-});
-
+/**
+ * Search the API for a cocktail using the search input
+ */
 function searchApi() {
   const searchInput = document.querySelector("#search-input").value;
   console.log(searchInput);
@@ -29,18 +29,16 @@ function searchApi() {
   // api link is www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita
   const apiLink = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchInput}`;
 
-  console.log(apiLink);
   fetch(apiLink)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
       const drinks = data.drinks;
-      console.log(drinks);
+      const drinkCounter = drinks.length;
       const outputDiv = document.querySelector("#output");
-      while (outputDiv.firstChild) {
-        outputDiv.removeChild(outputDiv.firstChild);
-      }
+
+      clearOutputDiv();
 
       drinks.forEach(function (drink) {
         const drinkName = drink.strDrink;
@@ -48,8 +46,6 @@ function searchApi() {
         const drinkId = drink.idDrink;
         const drinkInstructions = drink.strInstructions;
         const drinkIngredients = [];
-
-        console.log(drink);
 
         // get the ingredients and measurements and concatenate them
         for (let j = 1; j <= 15; j++) {
@@ -59,6 +55,7 @@ function searchApi() {
             drinkIngredients.push(`${measure} ${ingredient}`);
           }
         }
+
         const drinkDiv = document.createElement("div");
         const drinkNameElement = document.createElement("h2");
         drinkNameElement.textContent = drinkName;
@@ -82,4 +79,68 @@ function searchApi() {
         outputDiv.appendChild(drinkDiv);
       });
     });
+}
+
+/**
+ * Get a random cocktail from the API
+ */
+function randomApi() {
+  const apiLink = `https://www.thecocktaildb.com/api/json/v1/1/random.php`;
+
+  fetch(apiLink)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      const drinks = data.drinks;
+      const outputDiv = document.querySelector("#output");
+
+      clearOutputDiv();
+
+      drinks.forEach(function (drink) {
+        const drinkName = drink.strDrink;
+        const drinkImage = drink.strDrinkThumb;
+        const drinkId = drink.idDrink;
+        const drinkInstructions = drink.strInstructions;
+        const drinkIngredients = [];
+
+        // get the ingredients and measurements and concatenate them
+        for (let j = 1; j <= 15; j++) {
+          const ingredient = drink["strIngredient" + j];
+          const measure = drink["strMeasure" + j];
+          if (ingredient) {
+            drinkIngredients.push(`${measure} ${ingredient}`);
+          }
+        }
+
+        const drinkDiv = document.createElement("div");
+        const drinkNameElement = document.createElement("h2");
+        drinkNameElement.textContent = drinkName;
+        const drinkImageElement = document.createElement("img");
+        drinkImageElement.src = drinkImage;
+        drinkImageElement.alt = drinkName;
+        const drinkInstructionsElement = document.createElement("p");
+        drinkInstructionsElement.textContent = drinkInstructions;
+        drinkDiv.appendChild(drinkNameElement);
+        drinkDiv.appendChild(drinkImageElement);
+        drinkIngredients.forEach(function (ingredient) {
+          const drinkIngredientElement = document.createElement("p");
+          drinkIngredientElement.textContent = ingredient;
+          drinkDiv.appendChild(drinkIngredientElement);
+          drinkNameElement.classList.add("drinkName");
+          drinkIngredientElement.classList.add("drinkIngredients");
+          drinkInstructionsElement.classList.add("drinkInstructions");
+          drinkDiv.classList.add("drinkDiv");
+        });
+        drinkDiv.appendChild(drinkInstructionsElement);
+        outputDiv.appendChild(drinkDiv);
+      });
+    });
+}
+
+/**
+ * Clear the output div
+ */
+function clearOutputDiv() {
+  document.querySelector("#output").querySelectorAll("*").forEach(n => n.remove());
 }
