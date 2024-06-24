7a77478fe5c050eb03d7e45a1ba7cb0f82cc7f00
acegoal07 <39467245+acegoal07@users.noreply.github.com>
2024-06-24 20:03:04 +0100

    Less Jank

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,146 +1,142 @@
-// SEARCH BAR ENTER BUTTON //
-const searchForm = document.querySelector("#search-form");
-
-function performSearch(event) {
-  event.preventDefault();
-  const searchTerm = document.querySelector("#search-input").value.trim();
-  console.log("Searching for:", searchTerm);
-}
-
-searchForm.addEventListener("submit", performSearch);
-
 // ON LOAD
 window.addEventListener("load", function () {
-  // Clear search bar on refresh
-  document.querySelector("#search-input").value = "";
-  // Text search
-  document.querySelector("#search-button").addEventListener("click", searchApi);
-  // Random search
-  document.querySelector("#random-button").addEventListener("click", randomApi);
+   // Clear search bar on refresh
+   document.querySelector("#search-input").value = "";
+   // Text search
+   document.querySelector("#search-button").addEventListener("click", searchApi);
+   document.querySelector("#search-form").addEventListener("submit", (evt) => evt.preventDefault());
+   // Random search
+   document.querySelector("#random-button").addEventListener("click", randomApi);
 });
 
 /**
  * Search the API for a cocktail using the search input
  */
 function searchApi() {
-  const searchInput = document.querySelector("#search-input").value;
-  console.log(searchInput);
+   clearOutputDiv();
 
-  // api link is www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita
-  const apiLink = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchInput}`;
+   document.querySelector('#results-counter').classList.remove('hide');
 
-  fetch(apiLink)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      const drinks = data.drinks;
-      const drinkCounter = drinks.length;
-      const outputDiv = document.querySelector("#output");
+   // api link is www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita
+   fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${document.querySelector("#search-input").value.trim()}`)
+      .then(function (response) {
+         if (response.ok) {
+            return response.json();
+         } else {
+            throw new Error("No drinks found");
+         }
+      })
+      .then(function (data) {
+         const drinks = data.drinks;
 
-      clearOutputDiv();
-
-      drinks.forEach(function (drink) {
-        const drinkName = drink.strDrink;
-        const drinkImage = drink.strDrinkThumb;
-        const drinkId = drink.idDrink;
-        const drinkInstructions = drink.strInstructions;
-        const drinkIngredients = [];
-
-        // get the ingredients and measurements and concatenate them
-        for (let j = 1; j <= 15; j++) {
-          const ingredient = drink["strIngredient" + j];
-          const measure = drink["strMeasure" + j];
-          if (ingredient) {
-            drinkIngredients.push(`${measure} ${ingredient}`);
-          }
-        }
-
-        const drinkDiv = document.createElement("div");
-        const drinkNameElement = document.createElement("h2");
-        drinkNameElement.textContent = drinkName;
-        const drinkImageElement = document.createElement("img");
-        drinkImageElement.src = drinkImage;
-        drinkImageElement.alt = drinkName;
-        const drinkInstructionsElement = document.createElement("p");
-        drinkInstructionsElement.textContent = drinkInstructions;
-        drinkDiv.appendChild(drinkNameElement);
-        drinkDiv.appendChild(drinkImageElement);
-        drinkIngredients.forEach(function (ingredient) {
-          const drinkIngredientElement = document.createElement("p");
-          drinkIngredientElement.textContent = ingredient;
-          drinkDiv.appendChild(drinkIngredientElement);
-          drinkNameElement.classList.add("drinkName");
-          drinkIngredientElement.classList.add("drinkIngredients");
-          drinkInstructionsElement.classList.add("drinkInstructions");
-          drinkDiv.classList.add("drinkDiv");
-        });
-        drinkDiv.appendChild(drinkInstructionsElement);
-        outputDiv.appendChild(drinkDiv);
+         if (!drinks) {
+            document.querySelector('#results-counter-number').textContent = '0';
+         } else {
+            document.querySelector('#results-counter-number').textContent = drinks.length;
+            outputDataToDiv(drinks);
+         }
+      }).catch(error, () => {
+         throw new Error(error);
       });
-    });
 }
 
 /**
  * Get a random cocktail from the API
  */
 function randomApi() {
-  const apiLink = `https://www.thecocktaildb.com/api/json/v1/1/random.php`;
+   clearOutputDiv();
 
-  fetch(apiLink)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      const drinks = data.drinks;
-      const outputDiv = document.querySelector("#output");
+   const counter = document.querySelector('#results-counter');
+   if (!counter.classList.contains('hide')) {
+      counter.classList.add('hide');
+      document.querySelector('#results-counter-number').textContent = '0';
+   }
 
-      clearOutputDiv();
-
-      drinks.forEach(function (drink) {
-        const drinkName = drink.strDrink;
-        const drinkImage = drink.strDrinkThumb;
-        const drinkId = drink.idDrink;
-        const drinkInstructions = drink.strInstructions;
-        const drinkIngredients = [];
-
-        // get the ingredients and measurements and concatenate them
-        for (let j = 1; j <= 15; j++) {
-          const ingredient = drink["strIngredient" + j];
-          const measure = drink["strMeasure" + j];
-          if (ingredient) {
-            drinkIngredients.push(`${measure} ${ingredient}`);
-          }
-        }
-
-        const drinkDiv = document.createElement("div");
-        const drinkNameElement = document.createElement("h2");
-        drinkNameElement.textContent = drinkName;
-        const drinkImageElement = document.createElement("img");
-        drinkImageElement.src = drinkImage;
-        drinkImageElement.alt = drinkName;
-        const drinkInstructionsElement = document.createElement("p");
-        drinkInstructionsElement.textContent = drinkInstructions;
-        drinkDiv.appendChild(drinkNameElement);
-        drinkDiv.appendChild(drinkImageElement);
-        drinkIngredients.forEach(function (ingredient) {
-          const drinkIngredientElement = document.createElement("p");
-          drinkIngredientElement.textContent = ingredient;
-          drinkDiv.appendChild(drinkIngredientElement);
-          drinkNameElement.classList.add("drinkName");
-          drinkIngredientElement.classList.add("drinkIngredients");
-          drinkInstructionsElement.classList.add("drinkInstructions");
-          drinkDiv.classList.add("drinkDiv");
-        });
-        drinkDiv.appendChild(drinkInstructionsElement);
-        outputDiv.appendChild(drinkDiv);
+   fetch(`https://www.thecocktaildb.com/api/json/v1/1/random.php`)
+      .then(function (response) {
+         if (response.ok) {
+            return response.json();
+         } else {
+            throw new Error("No drinks found");
+         }
+      })
+      .then(function (data) {
+         outputDataToDiv(data.drinks);
+      }).catch(error, () => {
+         throw new Error(error);
       });
-    });
 }
 
 /**
  * Clear the output div
  */
 function clearOutputDiv() {
-  document.querySelector("#output").querySelectorAll("*").forEach(n => n.remove());
+   document.querySelector("#output").querySelectorAll("*").forEach(n => n.remove());
+}
+
+/**
+ * Takes the data from the API and outputs it to the div
+ * @param {Array} data The drinks array data from the API
+ */
+function outputDataToDiv(data) {
+   const outputDiv = document.querySelector("#output");
+
+   data.forEach(function (drink) {
+      const drinkName = drink.strDrink;
+      const drinkImage = drink.strDrinkThumb;
+      // const drinkId = drink.idDrink;
+      const drinkInstructions = drink.strInstructions;
+      const drinkIngredients = [];
+
+      // get the ingredients and measurements and concatenate them
+      for (let j = 1; j <= 15; j++) {
+         const ingredient = drink["strIngredient" + j];
+         const measure = drink["strMeasure" + j];
+         if (ingredient) {
+            drinkIngredients.push(`${measure} ${ingredient}`);
+         }
+      }
+
+      const drinkDiv = document.createElement("div");
+      drinkDiv.classList.add("drinkDiv");
+
+      const drinkNameElement = document.createElement("h2");
+      drinkNameElement.textContent = drinkName;
+      drinkNameElement.classList.add("drinkName");
+      drinkDiv.appendChild(drinkNameElement);
+
+      const drinkImageElement = document.createElement("img");
+      drinkImageElement.src = drinkImage;
+      drinkImageElement.alt = drinkName;
+      drinkDiv.appendChild(drinkImageElement);
+
+      const drinkIngredientsDiv = document.createElement("div");
+      const drinkIngredientsHeader = document.createElement("h3");
+      drinkIngredientsHeader.textContent = "Ingredients";
+      drinkIngredientsHeader.classList.add("drinkIngredientsHeader");
+      drinkIngredientsDiv.appendChild(drinkIngredientsHeader);
+
+      drinkIngredients.forEach(function (ingredient) {
+         const drinkIngredientElement = document.createElement("p");
+         drinkIngredientElement.textContent = ingredient;
+         drinkIngredientElement.classList.add("drinkIngredients");
+         drinkIngredientsDiv.appendChild(drinkIngredientElement);
+      });
+
+      drinkDiv.appendChild(drinkIngredientsDiv);
+
+      const drinkInstructionsDiv = document.createElement("div");
+      const drinkInstructionsHeader = document.createElement("h3");
+      drinkInstructionsHeader.textContent = "Instructions";
+      drinkInstructionsHeader.classList.add("drinkInstructionsHeader");
+      drinkInstructionsDiv.appendChild(drinkInstructionsHeader);
+
+      const drinkInstructionsElement = document.createElement("p");
+      drinkInstructionsElement.classList.add("drinkInstructions");
+      drinkInstructionsElement.textContent = drinkInstructions;
+      drinkInstructionsDiv.appendChild(drinkInstructionsElement);
+
+      drinkDiv.appendChild(drinkInstructionsDiv);
+      outputDiv.appendChild(drinkDiv);
+   });
 }
